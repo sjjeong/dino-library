988e1dfb122f875d066fb574c657c02936b1e9f0
sjjeong <sjjeong1225@gmail.com>
2019-06-09 17:07:07 +0900

    add setup koin setting

---
+++ src/main/kotlin/com/dino/library/ext/KoinExt.kt
@@ -0,0 +1,20 @@
+package com.dino.library.ext
+
+import android.content.Context
+import com.googry.dinolibrary.BuildConfig
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.logger.AndroidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.EmptyLogger
+import org.koin.core.module.Module
+
+fun setupKoin(
+    context: Context,
+    vararg module: Module
+) {
+    startKoin {
+        logger(if (BuildConfig.DEBUG) AndroidLogger() else EmptyLogger())
+        androidContext(context)
+        modules(*module)
+    }
+}
