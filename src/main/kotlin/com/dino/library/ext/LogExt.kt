e43fcd3a6b12c0c0243fa1ed18b8c4baa1fc1923
sjjeong <sjjeong1225@gmail.com>
2019-06-09 17:06:59 +0900

    add show call stack log

--- src/main/kotlin/com/dino/library/ext/LogExt.kt
+++ src/main/kotlin/com/dino/library/ext/LogExt.kt
@@ -1,9 +1,56 @@
 package com.dino.library.ext
 
 import android.util.Log
+import com.googry.dinolibrary.BuildConfig
 
-const val LOG_TAG = "googry"
+const val LOG_TAG = "dino_log"
 
-fun logE(msg: String) {
-    Log.e(LOG_TAG, msg)
+fun logV(msg: String, tag: String = LOG_TAG, showCallStack: Boolean = false) {
+    if (BuildConfig.DEBUG) {
+        if (showCallStack) {
+            Log.v(tag, msg, Throwable())
+        } else {
+            Log.v(tag, msg)
+        }
+    }
+}
+
+fun logD(msg: String, tag: String = LOG_TAG, showCallStack: Boolean = false) {
+    if (BuildConfig.DEBUG) {
+        if (showCallStack) {
+            Log.d(tag, msg, Throwable())
+        } else {
+            Log.d(tag, msg)
+        }
+    }
+}
+
+fun logI(msg: String, tag: String = LOG_TAG, showCallStack: Boolean = false) {
+    if (BuildConfig.DEBUG) {
+        if (showCallStack) {
+            Log.i(tag, msg, Throwable())
+        } else {
+            Log.i(tag, msg)
+        }
+    }
+}
+
+fun logW(msg: String, tag: String = LOG_TAG, showCallStack: Boolean = false) {
+    if (BuildConfig.DEBUG) {
+        if (showCallStack) {
+            Log.w(tag, msg, Throwable())
+        } else {
+            Log.w(tag, msg)
+        }
+    }
+}
+
+fun logE(msg: String, tag: String = LOG_TAG, showCallStack: Boolean = false) {
+    if (BuildConfig.DEBUG) {
+        if (showCallStack) {
+            Log.e(tag, msg, Throwable())
+        } else {
+            Log.e(tag, msg)
+        }
+    }
 }
