1a2e2eb0eee6da4d7a75d9e03731d92b49a238d1
sjjeong <sjjeong1225@gmail.com>
2019-10-13 22:47:54 +0900

    BaseFragment 개선

--- src/main/kotlin/com/dino/library/ui/BaseFragment.kt
+++ src/main/kotlin/com/dino/library/ui/BaseFragment.kt
@@ -1,48 +1,39 @@
 package com.dino.library.ui
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
+import com.dino.library.ext.showToast
 import com.googry.dinolibrary.BR
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 abstract class BaseFragment<B : ViewDataBinding, VM : BaseViewModel>(
-    @LayoutRes private val layoutResId: Int
-) : Fragment() {
+    layoutResId: Int,
+    viewModelCls: Class<VM>
+) : Fragment(layoutResId) {
 
-    protected lateinit var binding: B
-        private set
+    protected val binding by lazy { DataBindingUtil.bind<B>(view!!)!! }
 
-    abstract val viewModel: VM
+    protected val viewModel by viewModel(clazz = viewModelCls.kotlin)
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding = DataBindingUtil.inflate(inflater, layoutResId, container, false)
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
         binding {
-            binding.lifecycleOwner = this@BaseFragment
-            binding.setVariable(BR.vm, viewModel)
+            lifecycleOwner = viewLifecycleOwner
+            setVariable(BR.vm, viewModel)
         }
-        viewModel.run {
-            liveToast.observe(viewLifecycleOwner) {
-                showToast(it)
-            }
+        viewModel {
+            liveToast.observe(viewLifecycleOwner) { this@BaseFragment.showToast(it) }
         }
     }
 
     protected fun binding(action: B.() -> Unit) {
         binding.run(action)
+    }
+
+    protected fun viewModel(action: VM.() -> Unit) {
+        viewModel.run(action)
     }
 
 }
